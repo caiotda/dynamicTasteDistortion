177e353b82e870acf528fa52bb35eaca9602cd54
Caio Andrade <caiotdandrade@gmail.com>
2025-06-06 09:42:49 -0300

    chore: adds constants file

---
+++ constants.py
@@ -0,0 +1,12 @@
+MOVIELENS_PATH = "data/movielens-1m"
+
+
+ML_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m.csv"
+ML_RATINGS_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m_ratings.csv"
+ML_BIN_RATINGS_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m_bin_ratings.csv"
+FOOD_COM_PATH = "data/food"
+YELP_PATH = "data/yelp"
+
+
+SIMULATION_PATH = "data/simulation"
+ML_1M_FILLED_PATH = f"{SIMULATION_PATH}/movielens_1m_sinthetically_filled.csv"
