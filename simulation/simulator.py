8ef077b6b6186c895524b8d42c0521548102aa85
Caio Andrade <caiotdandrade@gmail.com>
2025-11-21 11:03:55 -0300

    Changes some function parameters and unloads constructor

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -1,20 +1,20 @@
 import torch
 
-from tqdm import tqdm
-
+import numpy as np
 import pandas as pd
 
 from calibratedRecs.metrics import mace
-from tasteDistortionOnDynamicRecs.simulation.simulationUtils import get_candidate_items, simulate_user_feedback, setup_user_timestamp_distribution, random_rec, get_feedback_for_predictions
+from tasteDistortionOnDynamicRecs.simulation.simulationUtils import random_rec, get_feedback_for_predictions
 from tasteDistortionOnDynamicRecs.simulationConstants import USER_COL, ITEM_COL, GENRES_COL
 from tasteDistortionOnDynamicRecs.simulation.tensorUtils import get_matrix_coordinates
 
+from bprMf.utils import train
 
-from bprMF.utils import train
+from tqdm import tqdm
 
 class Simulator:
-    def __init__(self, oracle_matrix, model, num_rounds, initial_date, user_sample=None):
-        self.timestamp_distribution = setup_user_timestamp_distribution()
+    def __init__(self, oracle_matrix, model, initial_date, user_timestamp_distribution, user_sample=None):
+        self.timestamp_distribution = user_timestamp_distribution
         self.user_idx_to_id = {idx: user_id for idx, user_id in enumerate(self.timestamp_distribution.keys())}
 
         if user_sample is None:
@@ -23,7 +23,6 @@ class Simulator:
             users = user_sample
         self.oracle_matrix = oracle_matrix[oracle_matrix[USER_COL].isin(users)]
         self.model = model
-        self.rounds = num_rounds
         self.initial_date = initial_date
 
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -82,15 +81,15 @@ class Simulator:
 
         timestamps = [(self.timestamp_distribution[user].rvs(1)[0] / 60) + self.initial_date for user in user_ids]
         entries = list(zip(users_indices, items, feedbacks, click_positions, timestamps))
-        interaction_df = pd.DataFrame(entries, columns=["user", "item", "feedback", "clicked_at", "timestamp"])
+        interaction_df = pd.DataFrame(entries, columns=["user", "item", "click", "clicked_at", "timestamp"])
 
 
-        interaction_df.loc[interaction_df["feedback"] != 1.0, "clicked_at"] = np.nan
-        interaction_df.loc[interaction_df["feedback"] != 1.0, "timestamp"] = np.nan
+        interaction_df.loc[interaction_df["click"] != 1.0, "clicked_at"] = np.nan
+        interaction_df.loc[interaction_df["click"] != 1.0, "timestamp"] = np.nan
 
         return interaction_df
 
-    def bootstrap_clicks(self, k=20):
+    def bootstrap_clicks(self, k=20, bootstrapping_rounds=5):
         """
         Given unique users and unique items, recommend up to k items to every user
         using a preference matrix as a relevancy model and using a click model
@@ -103,11 +102,10 @@ class Simulator:
         
         """
         
-        boostrapped_df = pd.DataFrame([], columns=["user", "item", "feedback", "clicked_at", "timestamp"])
-        for _ in range(self.rounds):
-            round_df = simulate_user_feedback(
+        boostrapped_df = pd.DataFrame([], columns=["user", "item", "click", "clicked_at", "timestamp"])
+        for _ in range(bootstrapping_rounds):
+            round_df = self.simulate_user_feedback(
                 mask=None,
-                model=None,
                 feedback_from_bootstrap=True,
                 k=k
             )
@@ -121,6 +119,7 @@ class Simulator:
         self,
         k=100,
         L=10,
+        rounds=10_000
     ):
         """
         Simulates a dynamic recommendation setting.
@@ -166,24 +165,23 @@ class Simulator:
         )
         user2history = self.click_matrix.groupby(USER_COL).agg({ITEM_COL: list}).to_dict()[ITEM_COL]
         
-        boostrapped_df = pd.DataFrame([], columns=["user", "item", "feedback", "clicked_at", "timestamp"])
+        boostrapped_df = self.click_matrix.copy()
         maces = []
-        for _ in range(self.rounds):
-            round_df = simulate_user_feedback(
+        for round_idx in tqdm(range(rounds), desc="Processing rounds..."):
+            round_df = self.simulate_user_feedback(
                 mask=None,
-                model=None,
                 feedback_from_bootstrap=True,
                 k=k
             )
-            if (round % L == 0):
+            if (round_idx % L == 0):
                 print("retraining model...")
-                model = train(model, boostrapped_df)
+                self.model = train(self.model, boostrapped_df)
                 print("Calculating mace")
                 rec_df_grouped = boostrapped_df.groupby(USER_COL).agg({ITEM_COL: list}).reset_index().rename(columns={ITEM_COL: "rec"})
                 iteration_mace = mace(df=rec_df_grouped, user2history=user2history, recCol='rec', item2genreMap=item2genreMap)
                 maces.append(iteration_mace)
-            if (round % 100 == 0):
-                boostrapped_df.to_csv(f"data/movielens/no_calibration_sim_up_to_round_{round}")
+            if (round_idx % 100 == 0):
+                boostrapped_df.to_csv(f"data/movielens/no_calibration_sim_up_to_round_{round_idx}")
             boostrapped_df = pd.concat([boostrapped_df, round_df], ignore_index=True)
 
-        return boostrapped_df, maces   
+        return boostrapped_df, maces 
