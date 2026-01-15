03528c7b511bb9db785f074915a62adc0313bb4c
Caio Andrade <caiotdandrade@gmail.com>
2026-01-15 15:11:42 -0300

    Simplifies constants and fixes path now that this is a package

--- dynamicTasteDistortion/simulationConstants.py
+++ dynamicTasteDistortion/simulationConstants.py
@@ -4,30 +4,15 @@ input_size_to_file_name = {
     "l": "20m",
 }
 
+MOVIELENS_PATH = "dynamicTasteDistortion/data/movielens"
+STEAM_PATH = "dynamicTasteDistortion/data/steam"
+YELP_PATH = "dynamicTasteDistortion/data/yelp"
 
-MOVIELENS_PATH = "data/movielens"
-
-
-ML_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m.csv"
-ML_RATINGS_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m_ratings.csv"
-ML_BIN_RATINGS_DATA_PATH = f"{MOVIELENS_PATH}/movielens_1m_bin_ratings.csv"
-STEAM_PATH = "data/steam"
-YELP_PATH = "data/yelp"
-
-ARTIFACTS_PATH = "artifacts"
+ARTIFACTS_PATH = "dynamicTasteDistortion/artifacts"
 RESULTS_PATH = f"{ARTIFACTS_PATH}/results"
 MODEL_ARTIFACTS_PATH = f"{ARTIFACTS_PATH}/model"
 
-SIMULATION_PATH = "data/simulation"
-ML_1M_ORACLE_PATH = f"{SIMULATION_PATH}/movielens_1m_oracle.csv"
-ML_1M_FILLED_PATH = f"{SIMULATION_PATH}/movielens_1m_sinthetically_filled.csv"
-ML_1M_FILLED_PATH_PKL = f"{SIMULATION_PATH}/movielens_1m_sinthetically_filled.pkl"
-ML_1M_1K_SAMPLE_FILLED_PATH = (
-    f"{SIMULATION_PATH}/movielens_1m_1k_sample_sinthetically_filled.csv"
-)
-ML_1M_1K_SAMPLE_FILLED_PATH_PKL = (
-    f"{SIMULATION_PATH}/movielens_1m_1k_sample_sinthetically_filled.pkl"
-)
+SIMULATION_PATH = "dynamicTasteDistortion/data/simulation"
 
 USER_COL = "user"
 ITEM_COL = "item"
