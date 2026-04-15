73c83df126cd20735e73d85c32db74383aa9064b
Caio Andrade <caiotdandrade@gmail.com>
2026-04-15 15:43:17 -0300

    Allows plotting map and coverage

--- dynamicTasteDistortion/scripts/vis.py
+++ dynamicTasteDistortion/scripts/vis.py
@@ -53,11 +53,15 @@ def plot_metric_comparison(
     metric_map = {
         "mace": 0,
         "kl": 1,
+        "map": 2,
+        "coverage": 3,
     }
 
     metric_name_to_nice_name = {
         "mace": "Mean Average Calibration Error",
         "kl": "Average KL Divergence",
+        "map": "Mean Average Precision",
+        "coverage": "Catalog Coverage"
     }
 
     metric_index = metric_map[metric_name.lower()]
