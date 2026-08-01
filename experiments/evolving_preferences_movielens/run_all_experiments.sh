7b7e8ba9614df6e5bf86df71933b15aa2e020273
Caio Andrade <caiotdandrade@gmail.com>
2026-08-01 12:04:59 -0300

    Fix experiment name

--- experiments/evolving_preferences_movielens/run_all_experiments.sh
+++ experiments/evolving_preferences_movielens/run_all_experiments.sh
@@ -7,7 +7,7 @@ for exp in calibrated uncalibrated; do
 
   for i in $(seq 1 9); do
     echo "Running experiment ${exp}_alpha_0_$i"
-    simulate-recommendation --exp_file experiments/evolving_preferences_globo/bpr_unbiased_${exp}_alpha_0_${i}.yaml \
+    simulate-recommendation --exp_file experiments/evolving_preferences_movielens/bpr_unbiased_${exp}_alpha_0_${i}.yaml \
       || echo "${exp}_alpha_0_$i FAILED" >> failed_experiments.log
   done
 done
